f3ae22379c1c3b639fe2c167ce3b567a0740aa33
ahmedzafar0 <talhazafar0@gmail.com>
2020-12-15 01:57:53 +0500

    Update testing.js

--- JavaScript Compiler/testing.js
+++ JavaScript Compiler/testing.js
@@ -1,17 +1,51 @@
-const fs = require("fs");
-const tokenizer = require( 'wink-tokenizer' );
-// Create it's instance.
-var myTokenizer = tokenizer();
- 
-// Tokenize a tweet.
-var source = fs.readFileSync("mytext.txt").toString().toLowerCase()
-var tokens = myTokenizer.tokenize(source);
-console.log(tokens);
+var str = 'my name, is ahmed.hello() osama contributor why.me()'
 
-fs.writeFile('./tokens.txt', JSON.stringify(tokens, null, 2),'utf8' , function(err, data) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("data written");
+var rgx = /([a-z]+\.[a-z]+)/g;
+var space = /\s+/g
+var dot = /\./g
+var comma = /\,/g
+var openparen = /\(/g
+var closedparen = /\)/g
+var newline = /\n/g
+var notword = /\W/g
+var arr = []
+
+var str_arr = str.split(space)
+console.log(str_arr)
+
+for(var i = 0; i < str.length; i++){
+    
+    var character = str.charAt(i)
+    // console.log(str.charAt(i))
+    if(space.test(character)){
+        console.log('white space found' + ' ' + character)
+
     }
-});
+    if(dot.test(character)){
+        console.log('dot found' + ' ' + character)
+        
+    }
+    if(comma.test(character)){
+        console.log('comma found' + ' ' + character)
+        
+    }
+    if(openparen.test(character)){
+        console.log('openparen found' + ' ' + character)
+        
+    }
+    if(closedparen.test(character)){
+        console.log('closedparen found' + ' ' + character)
+        
+    }
+    if(newline.test(character)){
+        console.log('closedparen found' + ' ' + character)
+        
+    }
+
+}
+
+    // console.log('not a word found' + ' ' + character)
+    var word = str.split(notword)
+    console.log(word.toString())
+
+
