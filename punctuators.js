0a289eaa72b91b1d6c77854395692fa71e2892ae
osamakhan99 <osama021khan@gmail.com>
2020-12-14 16:19:30 +0500

    JavaScript Compiler

---
+++ punctuators.js
@@ -0,0 +1,13 @@
+const Regex = {
+// comments : { single: new RegExp('/^//'), multi: new RegExp('/^\/\*|\*\//')},
+ idRegex : new RegExp('(([A-Za-z]+|(_[A-Za-z0-9]))+([A-Za-z0-9]*))'),
+ intRegex : new RegExp('([+-]?[0-9]+)'),
+ floatRegex : new RegExp('([+-]?([0-9]*)?[.][0-9]+)'),
+ stringRegex : new RegExp('[\"]+([\\w]*|[\\W&&[^\\\\]]*|(\\\\[\\\\\"nt])*|(\\\\[\\\\])*)*[\"]+'),
+ charRegex : new RegExp('[\']+[\\w]'),
+ punctRegex : new RegExp('(\.|\,|\(|\)|\{|\}|\[|\]|\:)')
+ //punct: [ ".",",","(",")","{","}","[","]",";",":" ],
+ //oprRegex = new RegExp('/^(\+|-|\*|\/|=|\>|\<|\>=|\<=|\&&|\+=|\-=|\*=|\/=|\||\%|\!|\^|\(|\))$/')
+ //oprRegex = new RegExp('(\+|\-|\*|\/|=|%|!|&)')
+}
+exports.Regex = Regex
